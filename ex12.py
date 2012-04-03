126d9c5a3136d374882b4b6df7715864b6c058e8
Peter Dailey <peteradailey@gmail.com>
2012-04-03 11:37:46 -0700

    ex12 effecient I/O with raw_input

---
+++ ex12.py
@@ -0,0 +1,8 @@
+# Note the comma after the print statements to keep input on the same line as the question.
+
+age = raw_input("How old are you?")
+height = raw_input("How tall are you?")
+weight = raw_input("How much do you weigh?")
+
+print "So, you tell me you are %r old, %r tall, and %r heavy..." % (age, height, weight)
+print "Wanna date?"
