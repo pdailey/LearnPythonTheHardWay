e2e088445a449580b58d500057e8e0d2058f3d4c
Peter Dailey <peteradailey@gmail.com>
2012-04-03 11:08:31 -0700

    ex7 more strings

---
+++ ex7.py
@@ -0,0 +1,18 @@
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." * 10  # *10 prints the line 10 times
+
+end1 = "S"
+end2 = "E"
+end3 = "A"
+end4 = "O"
+end5 = "I"
+end6 = "L"
+
+# Watch that comma at the end. Try removing it to see what happens
+print end1 + end2 + end3,
+print end4 + end5 + end6
+
+print end1 + end2 + end3
+print end4 + end5 + end6
