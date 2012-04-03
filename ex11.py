ef5d16e622b96f298907a5c3c4f808963de5daea
Peter Dailey <peteradailey@gmail.com>
2012-04-03 11:28:42 -0700

    ex11 User I/O

---
+++ ex11.py
@@ -0,0 +1,11 @@
+# Note the comma after the print statements to keep input on the same line as the question.
+
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you tell me you are %r old, %r tall, and %r heavy..." % (age, height, weight)
+print "Wanna date?"
