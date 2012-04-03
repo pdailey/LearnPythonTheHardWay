0aa4c2a7c098828b314025d87723fae5c0e91478
Peter Dailey <peteradailey@gmail.com>
2012-04-03 11:13:54 -0700

    ex9 more string magic!

---
+++ ex9.py
@@ -0,0 +1,11 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something about those triple quotes. 
+They really get my heart racing!
+Seems like I can type with impunity!
+"""
