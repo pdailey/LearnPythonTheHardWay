cfb38273ddd7a0c316080da88b4ede6deea162b8
Peter Dailey <peteradailey@gmail.com>
2012-04-03 10:53:40 -0700

    ex5 Learning advanced string composition with variables

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Peter Dailey'
+my_age =  20
+my_height = 1 #self-defined unit
+my_weight = 165 #lbs
+
+my_eyes = 'Blue'
+my_teeth = 'white'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d Peter's tall." % my_height
+print "He's %d pounds." % my_weight
+print "He's got %s eyes and %s hair." %(my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee" % my_teeth
+
+# Here comes the tricky line
+print "If I add %d, %d, and %d I get %d." %(my_age, my_height, my_weight, my_age + my_height + my_weight)
