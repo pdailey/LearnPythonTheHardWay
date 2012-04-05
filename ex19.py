c4b2e4ad0c0fda7535a9db47e977b7f42dd88c60
Peter Dailey <peteradailey@gmail.com>
2012-04-04 18:16:06 -0700

    ex19 & 20, combining functions and files

---
+++ ex19.py
@@ -0,0 +1,8 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+  print "You have %d chesses!" % cheese_count
+  print "You have %d boxes of crackers!" % boxes_of_crackers
+  print "Man, that's enough for a party!"
+  print "Get a blanket."
+
+print "We can just give the function numbers directly."
+cheese_and_crackers(20, 30)
